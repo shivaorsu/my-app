12ad1aa33ffe68596fcc3f7efac669748a8d01bb
shivaorsu <shivaorsu785@gmail.com>
2022-11-14 15:20:01 +0530

    Added Expenseitem

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,17 @@
+function ExpenseItem(){
+    return (
+      <div>
+        <h2>Expense Item</h2>
+        <div>
+            <p>Food</p>
+            <div>Rs 10</div>
+            <p>Petrol</p>
+            <div>Rs 10</div>
+            <p>Movie</p>
+            <div>Rs 200</div>
+        </div>
+      </div>
+    );
+}
+
+export default ExpenseItem;
