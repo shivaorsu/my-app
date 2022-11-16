f734f626b71de568c4e5d9f5ab18b8a01602775a
shivaorsu <shivaorsu785@gmail.com>
2022-11-16 14:53:46 +0530

    push ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,6 +3,9 @@ import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const clickHandler = () =>{
+       console.log('Clicked!!');
+  };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -10,10 +13,10 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
 
 export default ExpenseItem;
-
 
