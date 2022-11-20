a2ca8baf64ea790c9f72efd35d6b313b8302d807
shivaorsu <shivaorsu785@gmail.com>
2022-11-20 10:13:02 +0530

    push

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import NewExpense from './components/NewExpense';
   const DUMMY_EXPENSE= [
     {
       id: 'e1',
-      title: 'Toilet Paper',
+      title: 'Water',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', 
+     title: 'Fuel', 
+     amount: 799.49, 
+     date: new Date(2021, 2, 12) },
     {
       id: 'e3',
-      title: 'Car Insurance',
+      title: 'Iphone',
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
-      title: 'New Desk (Wooden)',
+      title: 'Mac Pro',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
